d2ccc67a3d98b21767612603402a09afd92a0040
Samuel Oh <samuel@Samuels-MacBook-Pro.local>
2019-01-15 15:44:50 -0800

    Initial express server with route files

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://sam:sam123@ds257314.mlab.com:57314/sameeoh_devconnector"
+};
---
+++ routes/api/posts.js
@@ -0,0 +1,10 @@
+// User Posts and Comments
+const express = require("express");
+const router = express.Router();
+
+// @route   GET api/posts/test
+// @desc    Tests post route
+// @access  Public
+router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
+
+module.exports = router;
